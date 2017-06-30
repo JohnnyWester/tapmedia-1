09f761a7dc99ef3db3c0ae00d1ddda614f2d84bd
romario1405 <romchik1405@ukr.net>
2017-06-30 23:31:47 +0300

    Init repository

---
+++ migrations/m170630_203008_create_table_click.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m170630_203008_create_table_click extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170630_203008_create_table_click cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170630_203008_create_table_click cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
