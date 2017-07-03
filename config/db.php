567bda02c11412a94a618a6a43eba5b8a0bdd6a2
romario1405 <romchik1405@ukr.net>
2017-07-03 22:49:15 +0300

    Disable db config

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tapmedia_test',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
